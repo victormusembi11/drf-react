32c91ac3d30842f7ae9f68f2c3946aec31f091fd
victormusembi11 <victor.musembi11@gmail.com>
2024-01-02 14:45:12 +0300

    added bootstrap navbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,22 +1,31 @@
 import { Outlet, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-const Layout = () => {
+export default function Layout() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/snippets">Snippets</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">Snippets App</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">
+                <Link to="/">Home</Link>
+              </Nav.Link>
+              <Nav.Link href="#snippets">
+                <Link to="/snippets">Snippets</Link>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
-      <Outlet />
+      <Container className="mt-3">
+        <Outlet />
+      </Container>
     </>
   );
-};
-
-export default Layout;
+}
